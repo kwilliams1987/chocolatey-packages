7dbc1cba74f80fad212aaa9821b803e3e2627280
Kristian Williams <kristian@softtech.nl>
2019-08-02 10:43:05 +0200

    * Fixed installer failure * Removed unnecessary shims.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,1 +1,11 @@
-Install-ChocolateyPackage 'Nvidia GeForce NOW' 'exe' '' 'https://download.nvidia.com/gfnpc/GeForceNOW-release.exe' 'https://download.nvidia.com/gfnpc/GeForceNOW-release.exe' 'b90eb039ff3f54a1f624fc8acd0fed35922574b44119a64fae935263da703bb0' 'b90eb039ff3f54a1f624fc8acd0fed35922574b44119a64fae935263da703bb0' 'sha256'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyWebFile 'Nvidia GeForce NOW' "$toolsDir\GeForceNOW-release.exe" -Url 'https://download.nvidia.com/gfnpc/GeForceNOW-release.exe' -Checksum 'b90eb039ff3f54a1f624fc8acd0fed35922574b44119a64fae935263da703bb0' -ChecksumType 'sha256'
+Get-ChocolateyUnzip "$toolsDir\GeForceNOW-release.exe" $toolsDir
+
+Install-ChocolateyInstallPackage 'Nvidia GeForce NOW' 'exe' '' "$toolsDir\setup.exe"
+
+$shimIgnores = @("GeForceNOW-release.exe", "GeforceNOW\GeforceNow_Installer.exe", "GeforceNOW\CEF\GeForceNOWReliabilityMonitor.exe", "GeforceNOW\CEF\GeForceNOWStreamer.exe", "NVI2\NVNetworkService.exe")
+foreach ($ignore in $shimIgnores) {
+    New-Item "$toolsDir\$ignore.ignore" -type file -force | out-null
+}
