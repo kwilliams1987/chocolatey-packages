89878d04e6d06ec1e948c39b6275b72934378f44
Kristian Williams <kristian@softtech.nl>
2019-11-25 10:27:18 +0100

    Checksum update.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Get-ChocolateyWebFile 'Nvidia GeForce NOW' "$toolsDir\GeForceNOW-release.exe" -Url 'https://download.nvidia.com/gfnpc/GeForceNOW-release.exe' -Checksum 'a17407940a05bde2b8367ced532e7318acbaf0c670af75c998dc26d5f897cabf' -ChecksumType 'sha256'
+Get-ChocolateyWebFile 'Nvidia GeForce NOW' "$toolsDir\GeForceNOW-release.exe" -Url 'https://download.nvidia.com/gfnpc/GeForceNOW-release.exe' -Checksum '86c3c96df454285203b3a46dfb70dea8fba6573a0ff12973f5f6d3d0df2b6e2b' -ChecksumType 'sha256'
 Get-ChocolateyUnzip "$toolsDir\GeForceNOW-release.exe" $toolsDir
 
 Install-ChocolateyInstallPackage 'Nvidia GeForce NOW' 'exe' '' "$toolsDir\setup.exe"
