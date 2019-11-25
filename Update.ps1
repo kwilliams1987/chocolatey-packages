f22cc42384c8a58fb1b058afb4d302ff599fedd6
Kristian Williams <kristian@softtech.nl>
2019-11-25 10:42:51 +0100

    Added auto-push to Chocolatey public repo support.

--- Update.ps1
+++ Update.ps1
@@ -34,4 +34,12 @@ if ($local -eq $online) {
 
     Write-Host "Repackaging...";
     .\pack.cmd
+
+    $apiKey = Read-Host "Enter a valid Chocolatey API Key to publish:";
+
+    if ($apiKey -ne "") 
+    {
+        Write-Host "Pushing new version...";
+        .\choco push "nvidia-geforce-now.$newver.nupkg" --source=https://chocolatey.org/ --apikey=$apiKey
+    }
 }
