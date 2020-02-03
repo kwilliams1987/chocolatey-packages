45a004258c74b682efef8d75c82309b3007cdfc8
Kristian Williams <kristian@softtech.nl>
2020-02-03 13:39:21 +0100

    - Shim ignores.:   - Added `GeforceNOW\CEF\GeForceNOWContainer.exe`   - Re-formatted as list for readabilty.

--- manual/nvidia-geforce-now/tools/chocolateyInstall.ps1
+++ manual/nvidia-geforce-now/tools/chocolateyInstall.ps1
@@ -5,7 +5,15 @@ Get-ChocolateyUnzip "$toolsDir\GeForceNOW-release.exe" $toolsDir
 
 Install-ChocolateyInstallPackage 'Nvidia GeForce NOW' 'exe' '' "$toolsDir\setup.exe"
 
-$shimIgnores = @("GeForceNOW-release.exe", "GeforceNOW\GeforceNow_Installer.exe", "GeforceNOW\CEF\GeForceNOWReliabilityMonitor.exe", "GeforceNOW\CEF\GeForceNOWStreamer.exe", "NVI2\NVNetworkService.exe")
+$shimIgnores = @(
+    "GeForceNOW-release.exe",
+    "GeforceNOW\GeforceNow_Installer.exe",
+    "GeforceNOW\CEF\GeForceNOWContainer.exe",
+    "GeforceNOW\CEF\GeForceNOWReliabilityMonitor.exe",
+    "GeforceNOW\CEF\GeForceNOWStreamer.exe",
+    "NVI2\NVNetworkService.exe"
+)
+
 foreach ($ignore in $shimIgnores) {
     New-Item "$toolsDir\$ignore.ignore" -type file -force | out-null
 }
