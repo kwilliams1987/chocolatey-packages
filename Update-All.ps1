62319cc1c040382e5adcdc0322ddf1af47a3449a
Kristian Williams <kristian@softtech.nl>
2020-03-16 15:17:48 +0100

    - Made auto updater iterations more clear.

--- Update-All.ps1
+++ Update-All.ps1
@@ -1,6 +1,8 @@
 $updaters = Get-ChildItem *\Update.ps1 -Recurse
 
 foreach ($updater in $updaters) {
+    $project = $updater.Directory.Name
+    Write-Host "Checking for $project updates..."
     Set-Location $updater.Directory
     .\Update.ps1
 }
