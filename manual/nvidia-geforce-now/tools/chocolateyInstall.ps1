62e8046c0680acac572e88b07e717c4c571aacb4
Kristian Williams <kristian@softtech.nl>
2020-03-30 13:07:04 +0200

    - Geforce NOW v2.0.18.53

--- manual/nvidia-geforce-now/tools/chocolateyInstall.ps1
+++ manual/nvidia-geforce-now/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Get-ChocolateyWebFile 'Nvidia GeForce NOW' "$toolsDir\GeForceNOW-release.exe" -Url 'https://download.nvidia.com/gfnpc/GeForceNOW-release.exe' -Checksum '18e19ecc1e8506e31a8ce5099ccbe420c1a3f99f7a52886fe167ccda62646732' -ChecksumType 'sha256'
+Get-ChocolateyWebFile 'Nvidia GeForce NOW' "$toolsDir\GeForceNOW-release.exe" -Url 'https://download.nvidia.com/gfnpc/GeForceNOW-release.exe' -Checksum 'ec9a8c005f8adaf26e1eadbe746217e7aff556a54301cd7fb4f145b432523ffc' -ChecksumType 'sha256'
 Get-ChocolateyUnzip "$toolsDir\GeForceNOW-release.exe" $toolsDir
 
 Install-ChocolateyInstallPackage 'Nvidia GeForce NOW' 'exe' '' "$toolsDir\setup.exe"
