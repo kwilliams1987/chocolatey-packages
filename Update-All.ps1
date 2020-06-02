6ebad7aadc6ac8b69d3849f9df6da071937c99d0
Kristian Williams <kristian.andersonwilliams@gmail.com>
2020-06-02 09:52:13 +0200

    Added CHOCO_API_KEY environment variable option.

--- Update-All.ps1
+++ Update-All.ps1
@@ -1,5 +1,18 @@
 param($apikey);
 
+if ($null -ne $apikey -and $null -ne $env:CHOCO_API_KEY) {
+    Write-Host "Overriding API Key from CHOCO_API_KEY environment variable with parameter." -ForegroundColor Yellow
+}
+
+if ($null -eq $apikey -and $null -ne $env:CHOCO_API_KEY) {
+    Write-Host "Using API Key from CHOCO_API_KEY environment variable." -ForegroundColor Yellow
+    $apikey = $env:CHOCO_API_KEY
+}
+
+if ($null -eq $apikey -and $null -eq $env:CHOCO_API_KEY) {
+    Write-Host "No API key available, updated packages will not be published." -ForegroundColor Orange
+}
+
 $source = Get-Location
 $updaters = Get-ChildItem *\Update.ps1 -Recurse
 
