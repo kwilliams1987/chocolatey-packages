d062f801c9b63a5bc950a81ba7851b998959f39e
Kristian Williams <kristian@softtech.nl>
2020-08-03 12:34:38 +0200

    Remove git commit code.

--- Update-All.ps1
+++ Update-All.ps1
@@ -13,19 +13,6 @@ if ($null -eq $choco -and $null -eq $env:CHOCO_API_KEY) {
     Write-Host "No Chocolatey API key available, updated packages will not be published." -ForegroundColor DarkYellow
 }
 
-if ($null -ne $github -and $null -ne $env:GITHUB_API_KEY) {
-    Write-Host "Overriding API Key from GITHUB_API_KEY environment variable with parameter." -ForegroundColor Yellow
-}
-
-if ($null -eq $github -and $null -ne $env:GITHUB_API_KEY) {
-    Write-Host "Using API Key from GITHUB_API_KEY environment variable." -ForegroundColor Yellow
-    $github = $env:GITHUB_API_KEY
-}
-
-if ($null -eq $github -and $null -eq $env:GITHUB_API_KEY) {
-    Write-Host "No Github API key available, updated packages will not be commited." -ForegroundColor DarkYellow
-}
-
 $source = Get-Location
 $updaters = Get-ChildItem *\Update.ps1 -Recurse
 $updates = @();
@@ -42,17 +29,6 @@ foreach ($updater in $updaters) {
 
     if ($update -gt 1) {
         Write-Host "The updater reported error code $update." -ForegroundColor DarkYellow
-    }
-}
-
-if ($updates.Count -gt 0) {
-    Write-Host "One or more packages were updated, preparing a Github deploy." -ForegroundColor Yellow
-
-    if ($null -eq $github) {
-        Write-Host "No Github API key is available, skipping commit." -ForegroundColor DarkYellow;
-    } else {
-        & git add .
-        & git commit -m "Updated packages: $updates"
     }
 }
 
