3bb6dc48b4835ca350b8e31abc52c61a5466e60d
Kristian Williams <kristian.andersonwilliams@gmail.com>
2020-09-30 19:34:13 +0200

    Changed download method to match self-contained exe.

--- manual/ajour/tools/chocolateyInstall.ps1
+++ manual/ajour/tools/chocolateyInstall.ps1
@@ -1,3 +1,4 @@
-Install-ChocolateyPackage "Project Aurora" "exe" "/silent" `
-    "https://github.com/casperstorm/ajour/releases/download/0.3.4/ajour.exe" `
-    -checksum "f2386f07ee5f90fe30279a4c996cd48490222cd9b8dc79260c92fa177371718a" -checksumType "sha256"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+Get-ChocolateyWebFile -PackageName 'Ajour' -FileFullPath "$toolsDir\ajour.exe" `
+    -Url 'https://github.com/casperstorm/ajour/releases/download/0.3.4/ajour.exe'
