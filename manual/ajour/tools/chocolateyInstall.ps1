2241fd69dc8ec7ec78076e5460c2b16c9a69a517
Kristian Williams <kris@softtech.nl>
2020-10-02 09:59:43 +0200

    - Added SHA256 hash checking to Ajour installer.

--- manual/ajour/tools/chocolateyInstall.ps1
+++ manual/ajour/tools/chocolateyInstall.ps1
@@ -1,9 +1,18 @@
+$expectedHash = "f2386f07ee5f90fe30279a4c996cd48490222cd9b8dc79260c92fa177371718a";
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)";
+$fileName = "$env:TEMP\" + [System.IO.Path]::GetRandomFileName();
 
-Remove-Item -Path "$toolsDir\ajour.exe" -Force -Confirm:$false -ErrorAction SilentlyContinue;
-
-Get-ChocolateyWebFile -PackageName 'Ajour' -FileFullPath "$toolsDir\ajour.exe" `
+Get-ChocolateyWebFile -PackageName 'Ajour' -FileFullPath "$fileName" `
     -Url 'https://github.com/casperstorm/ajour/releases/download/0.3.4/ajour.exe';
+
+$hash = (Get-FileHash "$fileName" -Algorithm SHA256).Hash.ToLower();
+
+if ($hash -ne $expectedHash) {
+    Remove-Item $fileName -Force -Confirm:$false -ErrorAction SilentlyContinue;
+    throw "The returned hash $hash does not match the expected one.";
+}
+
+Move-Item -Path "$fileName" -Destination "$toolsDir\ajour.exe" -Force;
 
 $shortcutPath = "$env:AllUsersProfile\Microsoft\Windows\Start Menu\Programs\Ajour.lnk";
 
