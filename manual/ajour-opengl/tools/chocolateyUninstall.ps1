a45e2399a7e27663f2f1f14e6a67a3ab00285c35
Kristian Williams <kris@softtech.nl>
2020-10-06 14:59:48 +0200

    # Ajour (OpenGL) - Installer now uses Install-ChocolateyShortcut instead of manual. - Fixed uninstall error when app is not running.

--- manual/ajour-opengl/tools/chocolateyUninstall.ps1
+++ manual/ajour-opengl/tools/chocolateyUninstall.ps1
@@ -1,6 +1,6 @@
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)";
 
-Get-Process -Name "ajour-opengl" | Stop-Process -Force
+Get-Process -Name "ajour-opengl" -ErrorAction SilentlyContinue | Stop-Process -Force
 
 Remove-Item -Path "$env:AllUsersProfile\Microsoft\Windows\Start Menu\Programs\Ajour (OpenGL).lnk" -Force -Confirm:$false -ErrorAction SilentlyContinue;
 Remove-Item -Path "$toolsDir\ajour-opengl.exe" -Force -Confirm:$false -ErrorAction SilentlyContinue;
