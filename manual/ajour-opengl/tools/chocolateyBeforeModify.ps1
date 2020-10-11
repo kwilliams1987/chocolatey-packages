7fcbe5ecdaf8640e6d65f342662f998b5edb92a5
Kristian Williams <kristian.andersonwilliams@gmail.com>
2020-10-11 14:50:31 +0200

    # Ajour (OpenGL) - Now force closes Ajour (OpenGL) during update.

---
+++ manual/ajour-opengl/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,1 @@
+Get-Process -Name "ajour-opengl" -ErrorAction SilentlyContinue | Stop-Process -Force
--- manual/ajour-opengl/tools/chocolateyUninstall.ps1
+++ manual/ajour-opengl/tools/chocolateyUninstall.ps1
@@ -1,6 +1,4 @@
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)";
 
-Get-Process -Name "ajour-opengl" -ErrorAction SilentlyContinue | Stop-Process -Force
-
 Remove-Item -Path "$env:AllUsersProfile\Microsoft\Windows\Start Menu\Programs\Ajour (OpenGL).lnk" -Force -Confirm:$false -ErrorAction SilentlyContinue;
 Remove-Item -Path "$toolsDir\ajour-opengl.exe" -Force -Confirm:$false -ErrorAction SilentlyContinue;
