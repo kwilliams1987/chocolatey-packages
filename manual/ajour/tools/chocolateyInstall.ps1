a8f6afeb849d5f91d0b97ad96b548a051e422cab
Kristian Williams <kris@softtech.nl>
2020-10-12 09:28:11 +0200

    # Ajour - Updated package to 0.4.1. - Fixed broken installer. - Added ability to force version generation.

--- manual/ajour/tools/chocolateyInstall.ps1
+++ manual/ajour/tools/chocolateyInstall.ps1
@@ -1,19 +1,11 @@
-$expectedHash = "183cb7a6fd7dc03d71bc5d1e3ef0aabba77ada29ff5543e1d90d4b7348dd76ea";
+$expectedHash = "81d9b9c5f4304d0112b4c55779c49f89fd8d0e91173cd24c442db1e832bde683";
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)";
 $fileName = "$env:TEMP\" + [System.IO.Path]::GetRandomFileName();
 
 Get-ChocolateyWebFile -PackageName 'Ajour' -FileFullPath "$fileName" `
-    -Url 'https://github.com/casperstorm/ajour/releases/download/0.4.0/ajour.exe';
-
-$hash = (Get-FileHash "$fileName" -Algorithm SHA256).Hash.ToLower();
-
-if ($hash -ne $expectedHash) 
-{
-    Remove-Item $fileName -Force -Confirm:$false -ErrorAction SilentlyContinue;
-    Write-Error "The returned hash $hash does not match the expected one.";
-    $host.SetShouldExit(-1);
-    return;
-}
+    -Url 'https://github.com/casperstorm/ajour/releases/download/0.4.1/ajour.exe' `
+    -Checksum $expectedHash `
+    -ChecksumType 'sha256';
 
 Move-Item -Path "$fileName" -Destination "$toolsDir\ajour.exe" -Force;
 
