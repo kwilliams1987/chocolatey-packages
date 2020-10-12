e01492f68a97536e3341d51f8cd8fb5ab2a5e0a7
Kristian Williams <kris@softtech.nl>
2020-10-12 09:28:48 +0200

    # Ajour (OpenGL) - Updated package to 0.4.1. - Fixed broken installer. - Added ability to force version generation.

--- manual/ajour-opengl/tools/chocolateyInstall.ps1
+++ manual/ajour-opengl/tools/chocolateyInstall.ps1
@@ -1,19 +1,11 @@
-$expectedHash = "bd92dd37df0f06d30814f0b07c6a6e0e3a1151631b7b374264017b2a5798f604";
+$expectedHash = "91e46767c8cb00177b425a3b8ec2a3a215a02fe40b7a6962558194f4740c709e";
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)";
 $fileName = "$env:TEMP\" + [System.IO.Path]::GetRandomFileName();
 
 Get-ChocolateyWebFile -PackageName 'Ajour' -FileFullPath "$fileName" `
-    -Url 'https://github.com/casperstorm/ajour/releases/download/0.4.0/ajour-opengl.exe';
-
-$hash = (Get-FileHash "$fileName" -Algorithm SHA256).Hash.ToLower();
-
-if ($hash -ne $expectedHash) 
-{
-    Remove-Item $fileName -Force -Confirm:$false -ErrorAction SilentlyContinue;
-    Write-Error "The returned hash $hash does not match the expected one.";
-    $host.SetShouldExit(-1);
-    return;
-}
+    -Url 'https://github.com/casperstorm/ajour/releases/download/0.4.1/ajour-opengl.exe' `
+    -Checksum $expectedHash `
+    -ChecksumType 'sha256';
 
 Move-Item -Path "$fileName" -Destination "$toolsDir\ajour-opengl.exe" -Force;
 
