e8c76bc450ba1e63ba2cafe08e30124d06e22c4e
GitHub Action <action@github.com>
2021-01-15 09:18:26 +0000

    Automatically updated I AM.

--- manual/i-am/tools/chocolateyInstall.ps1
+++ manual/i-am/tools/chocolateyInstall.ps1
@@ -1,2 +1,2 @@
 Install-ChocolateyPackage "I AM" "msi" "/silent" "https://download.voipit.nl/IAM/PC/IAM.msi" `
-    -Checksum "0000000000000000000000000000000000000000000000000000000000000000" -checksumType "sha256"
+    -Checksum "873ed4ba1f7f1b9d6d18aab4f79efdc53df6309973cef39ec38ac0e250fadcb3" -checksumType "sha256"
