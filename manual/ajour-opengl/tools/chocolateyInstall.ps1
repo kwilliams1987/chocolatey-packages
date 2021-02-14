e6c7d4763bc3f2ffeb5d0b73ba519a627ffa4ff5
GitHub Action <action@github.com>
2021-02-14 18:16:50 +0000

    Automatically updated Ajour (Open GL).

--- manual/ajour-opengl/tools/chocolateyInstall.ps1
+++ manual/ajour-opengl/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-$expectedHash = "1f648e94a1b183d236010be4e12c9daeef000b1e15a8a9e5fa5cc62dd9649e3d";
+$expectedHash = "4f91ee08a3e7c68f44d0fe8af9979fae15d50e144d08f6f3c0277b469c69478e";
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)";
 $fileName = "$env:TEMP\" + [System.IO.Path]::GetRandomFileName();
 
 Get-ChocolateyWebFile -PackageName 'Ajour (OpenGL)' -FileFullPath "$fileName" `
-    -Url 'https://github.com/casperstorm/ajour/releases/download/0.7.0/ajour-opengl.exe' `
+    -Url 'https://github.com/casperstorm/ajour/releases/download/0.7.1/ajour-opengl.exe' `
     -Checksum $expectedHash `
     -ChecksumType 'sha256';
 
