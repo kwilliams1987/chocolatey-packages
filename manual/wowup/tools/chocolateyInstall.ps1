84d5c9ca91f8dec5e8a8c07338d60896faf73108
Kristian Williams <kristian.andersonwilliams@gmail.com>
2021-05-29 17:10:28 +0200

    - Fixed SHA256 hash script in v2.3.0.

--- manual/wowup/tools/chocolateyInstall.ps1
+++ manual/wowup/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Install-ChocolateyPackage 'WowUp' "exe" "/silent" `
     'https://github.com/WowUp/WowUp/releases/download/v2.3.0/WowUp-Setup-2.3.0.exe' `
-    -Checksum "d021f26b20c6234a095e4e59109e0e982.3.089227e5d97740d2034b04426a32" -ChecksumType 'sha256';
+    -Checksum "d021f26b20c6234a095e4e59109e0e982528289227e5d97740d2034b04426a32" -ChecksumType 'sha256';
