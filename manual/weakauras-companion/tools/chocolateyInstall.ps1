d9e05137e7452e24224a3463f039128fb7c142e0
GitHub Action <action@github.com>
2022-07-15 01:56:44 +0000

    Automatically updated WeakAuras Companion.

--- manual/weakauras-companion/tools/chocolateyInstall.ps1
+++ manual/weakauras-companion/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Install-ChocolateyPackage "WeakAuras Companion" "exe" "/silent" `
-    "https://github.com/WeakAuras/WeakAuras-Companion/releases/download/v4.0.3/WeakAuras-Companion-Setup-4.0.3.exe" `
-    -checksum "f10e9182fa0ed306c33b282070f661a97ea391badccb304c8bfe37a6f6cea786" -checksumType "sha256"
+    "https://github.com/WeakAuras/WeakAuras-Companion/releases/download/v4.0.4/WeakAuras-Companion-Setup-4.0.4.exe" `
+    -checksum "abca1bf0ebaf1cb4f978caab3f58ff40e8dca78b0bea6a464f50ae144510f6ee" -checksumType "sha256"
