357d63a150e3f15d66b37abf7dc0bee21b34ac06
Kristian Williams <kristian.andersonwilliams@gmail.com>
2022-10-14 09:12:10 +0200

    # I AM - Removed unused files.

--- manual/i-am/README.md
+++ manual/i-am/README.md
@@ -1,4 +1,6 @@
 # RoutIT I AM
+**Deprecated**: Replaced by Webex.
+
 All in one VOIP Solution from Dutch ISP RoutIT.
 
 https://chocolatey.org/packages/i-am
--- manual/i-am/tools/chocolateyInstall.ps1
+++
@@ -1,2 +0,0 @@
-Install-ChocolateyPackage "I AM" "msi" "/quiet" "https://download.voipit.nl/IAM/PC/IAM.msi" `
-    -Checksum "bb0d9734343ca662f9a125db2b1b3e64818f47b104d4d426c11035e7b6043d0c" -checksumType "sha256"
