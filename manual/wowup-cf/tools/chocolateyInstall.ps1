fff635d8d1127d08b793560d2dbb79041fcb4d81
Kristian Williams <kristian.andersonwilliams@gmail.com>
2023-04-28 17:49:32 +0200

    # WowUp CF - Broken link.

--- manual/wowup-cf/tools/chocolateyInstall.ps1
+++ manual/wowup-cf/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Install-ChocolateyPackage 'WowUp' "exe" "/silent" `
-    'https://github.com/WowUp/WowUp.CF/releases/download/v2.9.2/WowUp-Setup-2.9.2.exe' `
+    'https://github.com/WowUp/WowUp.CF/releases/download/v2.9.2/WowUp-CF-Setup-2.9.2.exe' `
     -Checksum "9328a555805281193ce7c3fef53748c2fb5dbada26923b0fc1c79816683ca064" -ChecksumType 'sha256';
