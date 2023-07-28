f75a97a85bb88c3361288b1258c310a54534be30
Kristian Williams <kristian.andersonwilliams@gmail.com>
2023-07-28 11:33:26 +0200

    # Wowup-CF - Fixed broken checksum for v2.10.0.

--- manual/wowup-cf/tools/chocolateyInstall.ps1
+++ manual/wowup-cf/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Install-ChocolateyPackage 'WowUp' "exe" "/silent" `
     'https://github.com/WowUp/WowUp.CF/releases/download/v2.10.0/WowUp-CF-Setup-2.10.0.exe' `
-    -Checksum "de6f3f70790ca6f47f4301de22bb4aa44feaef414caba40458af1025fc1654b8" -ChecksumType 'sha256';
+    -Checksum "c235c32c4741f12fea58bff31ce2077387d03bc04f92d76dc7b870d9951caeb6" -ChecksumType 'sha256';
