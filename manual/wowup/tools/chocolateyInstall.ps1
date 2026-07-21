843d1f955bd937219e872083afc8b2d5df450078
GitHub Action <action@github.com>
2026-07-21 19:21:48 +0000

    Automatically updated WowUp.

--- manual/wowup/tools/chocolateyInstall.ps1
+++ manual/wowup/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Install-ChocolateyPackage 'WowUp' "exe" "/silent" `
-    'https://github.com/WowUp/WowUp/releases/download/v2.22.0/WowUp-Setup-2.22.0.exe' `
-    -Checksum "90d6a6d05c295335f5111a87c4fcb70fe17082e0f346bd2a144962df091359bf" -ChecksumType 'sha256';
+    'https://github.com/WowUp/WowUp/releases/download/v2.23.0/WowUp-Setup-2.23.0.exe' `
+    -Checksum "f0d42f4de19bcf94287cd4b327d1c7741514323f009ee4808b58f451fe073a59" -ChecksumType 'sha256';
