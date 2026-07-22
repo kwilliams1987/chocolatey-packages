d398489f3156ea021c99317e106eb8d56dbf6461
GitHub Action <action@github.com>
2026-07-22 08:28:48 +0000

    Automatically updated WowUp CurseForge.

--- manual/wowup-cf/tools/chocolateyInstall.ps1
+++ manual/wowup-cf/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Install-ChocolateyPackage 'WowUp' "exe" "/silent" `
-    'https://github.com/WowUp/WowUp.CF/releases/download/v2.22.0/WowUp-CF-Setup-2.22.0.exe' `
-    -Checksum "ab1d679c5a6705b18c61b1104d389faddb1ecaeeea1f6ea3ff3b83438c26c56a" -ChecksumType 'sha256';
+    'https://github.com/WowUp/WowUp.CF/releases/download/v2.23.0/WowUp-CF-Setup-2.23.0.exe' `
+    -Checksum "ab8ebf8c063a7ec30119730f9c3f7033970d44606cb1f14c996f986758f4e694" -ChecksumType 'sha256';
