65434d94556f98859c0c2f9c1900e2ddcd2a3098
GitHub Action <action@github.com>
2026-08-03 02:48:07 +0000

    Automatically updated NVIDIA GeForce NOW.

--- manual/nvidia-geforce-now/tools/chocolateyInstall.ps1
+++ manual/nvidia-geforce-now/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Get-ChocolateyWebFile 'Nvidia GeForce NOW' "$toolsDir\GeForceNOW-release.exe" -Url 'https://download.nvidia.com/gfnpc/GeForceNOW-release.exe' -Checksum 'b482e731b5f358738da115da32f2a93af5667da4ee202fcb372f0cbaf0f93e49' -ChecksumType 'sha256'
+Get-ChocolateyWebFile 'Nvidia GeForce NOW' "$toolsDir\GeForceNOW-release.exe" -Url 'https://download.nvidia.com/gfnpc/GeForceNOW-release.exe' -Checksum '57fa3821b759cb0305e37ee39c0ab5559e4bd87fd262335257c97429a861a357' -ChecksumType 'sha256'
 Get-ChocolateyUnzip "$toolsDir\GeForceNOW-release.exe" $toolsDir
 
 Install-ChocolateyInstallPackage 'Nvidia GeForce NOW' 'exe' '' "$toolsDir\setup.exe"
